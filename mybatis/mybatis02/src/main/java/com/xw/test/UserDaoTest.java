ffadedbb41a840faf0e0dbb37ee5104a2cd4df0b
xuwei <xuwei@cncloudsec.com>
2020-07-23 16:58:40 +0800

    init

--- mybatis/mybatis02/src/main/java/com/xw/test/UserDaoTest.java
+++ mybatis/mybatis02/src/main/java/com/xw/test/UserDaoTest.java
@@ -5,16 +5,20 @@ import org.apache.ibatis.session.SqlSession;
 import com.xw.dao.UserDao;
 import com.xw.pojo.User;
 import com.xw.util.MybatisUtils;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class UserDaoTest {
+
+    private static Logger logger = Logger.getLogger(UserDao.class);
 
     public static void main(String[] args) {
         getUserList();
     }
 
     public static void getUserList() {
+        logger.info("进入方法......................");
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         UserDao mapper = sqlSession.getMapper(UserDao.class);
         List<User> userList = mapper.getUserList();
@@ -22,5 +26,6 @@ public class UserDaoTest {
             System.out.println(user.toString());
         }
         sqlSession.close();
+        logger.info("结束方法......................");
     }
 }
