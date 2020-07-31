fac32c551b362dbd9965c8dac8bcafad4c114940
xuwei <xuwei@cncloudsec.com>
2020-07-31 15:07:13 +0800

    init

--- wechat/src/main/java/cn/com/rivercloud/wechat/common/exception/GlobalExceptionHandler.java
+++ wechat/src/main/java/cn/com/rivercloud/wechat/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package cn.com.rivercloud.wechat.common.exception;
 
 import cn.com.rivercloud.wechat.common.lang.JsonResponseBuilder;
 import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     // 表单参数验证
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public JSONObject handler(MethodArgumentNotValidException e) {
+        logger.error("参数异常============" ,e);
         BindingResult bindingResult = e.getBindingResult();
         ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
         return new JsonResponseBuilder().success(false).message(objectError.getDefaultMessage()).build();
@@ -27,7 +32,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = IllegalArgumentException.class)
     public JSONObject handler(IllegalArgumentException e) {
-        System.out.println(e);
+        logger.error("Assert异常：----------------{}", e);
         return new JsonResponseBuilder().success(false).message(e.getMessage()).build();
     }
 
@@ -36,7 +41,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = HttpMessageNotReadableException.class)
     public JSONObject handler(HttpMessageNotReadableException e) {
-        System.out.println(e);
+        logger.error("数据格式错误：----------------{}", e);
         return new JsonResponseBuilder().success(false).message("数据格式错误").build();
     }
 
@@ -44,14 +49,14 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = RuntimeException.class)
     public JSONObject handler(RuntimeException e) {
-        System.out.println(e);
+        logger.error("运行时异常：----------------{}", e);
         return new JsonResponseBuilder().success(false).message(e.getMessage()).build();
     }
 
     @ExceptionHandler(value = Exception.class)
     public JSONObject handler(Exception e) {
-        System.out.println(e);
-        return null;
+        logger.error("未知异常：----------------{}", e);
+        return new JsonResponseBuilder().success(false).message(e.getMessage()).build();
     }
 
 
