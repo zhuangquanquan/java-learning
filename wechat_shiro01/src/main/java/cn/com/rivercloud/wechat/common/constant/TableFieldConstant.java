6e05bfc9ee7e34be8362edecbcd19ab6930eaf5f
xuwei <xuwei@cncloudsec.com>
2020-08-04 20:32:08 +0800

    mybatis修改为mybatis-plus

---
+++ wechat_shiro01/src/main/java/cn/com/rivercloud/wechat/common/constant/TableFieldConstant.java
@@ -0,0 +1,7 @@
+package cn.com.rivercloud.wechat.common.constant;
+
+public class TableFieldConstant {
+
+    //用户表
+    public final static String user_table_filed_userName = "user_name";
+}
---
+++ wechat_shiro01/src/main/java/cn/com/rivercloud/wechat/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package cn.com.rivercloud.wechat.controller;
+
+import cn.com.rivercloud.wechat.common.lang.Result;
+import cn.com.rivercloud.wechat.common.util.RoleEnum;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//普通用户
+@RequiresRoles("customer")
+@RestController
+@RequestMapping("/user")
+public class CustomerController {
+
+    @GetMapping("/getMessage")
+    public Result getMessage() {
+        return Result.succ("您拥有【用户】权限，可以获得该接口的信息");
+    }
+}
